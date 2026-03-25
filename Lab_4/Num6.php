02e6e93bb822945174aac41e9099fc5faf55efa8
Artem Antonov PS-11 <onedgamecompany@gmail.com>
2026-03-25 03:05:49 +0300

    Refactoring is completed

--- Lab_4/Num6.php
+++ Lab_4/Num6.php
@@ -1,10 +1,4 @@
-<?php
-
-// function IsSumm(array $arr, int $index): bool {
-//     if ($index >= 2)
-//         return $arr[$index] === '+' && is_int($arr[$index - 1]) && is_int($arr[$index - 2]); 
-//     return FALSE;
-// }
+ <?php
 
 function ReduceArray(array &$arr, int &$index, int $countReduceEl) {
     $count = 0;
@@ -14,27 +8,99 @@ function ReduceArray(array &$arr, int &$index, int $countReduceEl) {
     OrderArray($arr);
 }
 
+function ClearArrayOfEmtyRows(array $arr) {
+    $tempArr = [];
+    foreach ($arr as $element) {
+        if ($element !== '')
+            $tempArr[] = $element;
+    }
+    $arr = $tempArr;
+}
+
+function CharToInt(string $char): int {
+    switch ($char) {
+        case '0':
+            return 0;
+        case '1':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+    }
+    return -1; 
+}
+
+function StrToInt(string $str): int {
+    $num = 0;
+    for ($i = 0; $i < LenghtStr($str); $i++) {
+        if ($str[$i] !== '-')
+            $num = $num * 10 + CharToInt($str[$i]);
+    }
+    if ($str[0] === '-')
+        $num *= -1;
+    return $num;
+}
+
+function ConvertInt(array &$arr) {
+    $tempArr = [];
+    foreach ($arr as $element) {
+        if ($element !== '+' && $element !== '-' && $element !== '*')
+            $tempArr[] = StrToInt($element);
+        elseif ($element === '+' || $element === '-' || $element === '*')
+            $tempArr[] = $element;
+    }
+    $arr = $tempArr;
+}
+
 function GetArrayFromStr(string $str): array {
     $arr = [];
     $indexArr = 0;
-    $indexStr = 1;
-    while ($indexStr !== strlen($str) - 1) {
-        if (($str[$indexStr - 1] === ' ' && $str[$indexStr] !== ' ') || ($str[$indexStr - 1] !== ' ' && $indexStr - 1 === 0)) {
-            $arr[] = $str[$indexStr];
-        }
-        elseif ($str[$indexStr - 1] !== ' ' && $str[$indexStr] !== ' ') {
+    $indexStr = 0;
+    $arr[] = '';
+    while ($indexStr !== LenghtStr($str)) {
+        if ($str[$indexStr] !== ' ') {
             $arr[$indexArr] .= $str[$indexStr];
+            
         }
-        elseif ($str[$indexStr - 1] !== ' ' && $str[$indexStr] === ' ') {
+        elseif ($str[$indexStr] === ' ') {
+            $arr[] = '';
             $indexArr++;
         }
         $indexStr++;
     }
+    ClearArrayOfEmtyRows($arr);
+    ConvertInt($arr);
     return $arr;
 }
 
 function LenghtArray(array $arr): int {
+    $count = 0;
+    foreach ($arr as $element)
+        $count++;
+    return $count;
+}
 
+function LenghtStr(string $str): int {
+    $tempStr = '';
+    $count = 0;
+    while ($tempStr !== $str) {
+        $tempStr .= $str[$count];
+        $count += 1;
+    }
+    return $count;
 }
 
 function OrderArray(array &$arr) {
@@ -44,40 +110,30 @@ function OrderArray(array &$arr) {
     $arr = $tempArr;
 }
 
-
-// $operands = [1, 2, '+', 1, 1, '+', '+'];
-// $operands = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '+', '+', '+', '+', '+', '+', '+', '+', '+', '+'];
-// $operands = [1, 2, '+', 3, 4, '+', '+', 5, 6, '+', 7, 8, '+', '+', '+', 9, 10, '+', 11, '+', '+'];
-
-$arr = GetArrayFromStr('1 2 3 +');
-foreach ($arr as $el) {
-    echo "{$el} | ";
+if ($_POST['str'] !== '') {
+    $operands = GetArrayFromStr($_POST['str']);
+    $index = 0;
+    while (LenghtArray($operands) !== 1) {
+        $l = count($operands);
+        echo "{$index} & {$l} |";
+        switch ($operands[$index]) {
+            case '+':
+                $operands[$index - 2] = $operands[$index - 2] + $operands[$index - 1];
+                ReduceArray($operands, $index, 2);
+                break;
+            case '-':
+                $operands[$index - 2] = $operands[$index - 2] - $operands[$index - 1];
+                ReduceArray($operands, $index, 2);
+                break;
+            case '*':
+                $operands[$index - 2] = $operands[$index - 2] * $operands[$index - 1];
+                ReduceArray($operands, $index, 2);
+                break;
+        }
+        $index++;
+    }
+    echo $operands[0];
 }
+else
+    echo 'EROR INPUT';
 
-
-
-
-
-
-
-// $operands = GetArrayFromStr('1 2 +');
-
-// $index = 0;
-// while (count($operands) !== 1) {  // LengthArray($operands) !== 1
-//     switch ($operands[$index]) {
-//         case '+':
-//             $operands[$index - 2] = $operands[$index - 2] + $operands[$index - 1];
-//             ReduceArray($operands, $index, 2);
-//             break;
-//         case '-':
-//             $operands[$index - 2] = $operands[$index - 2] - $operands[$index - 1];
-//             ReduceArray($operands, $index, 2);
-//             break;
-//         case '*':
-//             $operands[$index - 2] = $operands[$index - 2] + $operands[$index - 1];
-//             ReduceArray($operands, $index, 2);
-//             break;
-//     }
-//     $index++;
-// }
-// echo $operands[0];
